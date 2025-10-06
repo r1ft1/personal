ff2f2e16e04b40b50b0fa0f29963a737a25cccc4
James <jamessundaram8@gmail.com>
2025-10-06 17:28:07 +0100

    svg -> png to save data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-  import sticker from "$lib/assets/sticker.svg";
+  import sticker from "$lib/assets/sticker.png";
   import pic from "$lib/assets/pic.jpg";
 </script>
 
 <div
-  class="overflow-y-scroll xl:flex xl:justify-around xl:items-center bg-[#FEEDF4] xl:overflow-auto h-svh xl:h-screen snap-mandatory snap-y"
+  class="xl:w-full overflow-y-scroll xl:flex xl:justify-between xl:items-center bg-[#FEEDF4] xl:overflow-auto
+  h-svh xl:h-screen snap-mandatory snap-y"
 >
-  <div class=" flex-col h-svh xl:h-full flex items-center py-8 snap-center">
+  <div
+    class="flex-col h-svh xl:h-full py-8 flex items-center basis-full xl:basis-1/2 snap-center"
+  >
     <!-- <div class="flex py-4 flex-col items-center"> -->
     <!--   <svg -->
     <!--     xmlns="http://www.w3.org/2000/svg" -->
@@ -22,9 +25,9 @@
     <!-- </div> -->
     <!---->
     <div
-      class="flex flex-col items-center w-full h-1/2 mt-auto mb-auto justify-center gap-4"
+      class="flex flex-col items-center mt-auto mb-auto justify-center gap-4"
     >
-      <img class=" object-cover" alt="Oat Mocha Logo" src={sticker} />
+      <img class="object-contain w-1/2" alt="Oat Mocha Logo" src={sticker} />
       <p>website developer - leeds, UK</p>
 
       <p>oatmochadigital@gmail.com - Contact me!</p>
@@ -45,17 +48,19 @@
   </div>
 
   <div
-    class="bg-[#FEEDF4] flex-col gap-4 h-screen xl:h-1/4 xl:w-1/4 flex snap-center items-start justify-center px-4 py-8"
+    class="bg-[#FEEDF4] h-svh xl:h-full flex-col gap-4 xl:basis-1/2 flex snap-center items-center
+    justify-center px-4"
   >
     <p>about me</p>
     <p class="text-md">
       James - website creation and hosting for small businesses in north
       yorkshire
     </p>
-    <div class="object-cover xl:object-contain w-full flex gap-2">
-      <img src={pic} alt="Pic of the Author and Dog" />
-      <div class="flex flex-col justify-between"></div>
-    </div>
+    <img
+      src={pic}
+      class="object-cover xl:object-contain max-w-xs flex gap-2"
+      alt="Pic of the Author and Dog"
+    />
     <p>Billy (dog)</p>
   </div>
 </div>
